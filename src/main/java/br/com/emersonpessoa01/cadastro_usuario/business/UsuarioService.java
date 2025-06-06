df6d307cc7e5bc67ecc7960e036f88fcec5b789b
emersonpessoa <emersonpessoa05102008@gmail.com>
2025-06-06 18:53:39 -0300

    feat: Added atualizarUsuarioPorId :rocket:

--- src/main/java/br/com/emersonpessoa01/cadastro_usuario/business/UsuarioService.java
+++ src/main/java/br/com/emersonpessoa01/cadastro_usuario/business/UsuarioService.java
@@ -17,14 +17,29 @@ public class UsuarioService {
         repository.saveAndFlush(usuario);
         return usuario;
     }
+
     public Usuario buscarUsuarioPorEmail(String email) {
-        return repository.findByEmail(email).orElseThrow(()-> new RuntimeException("Usuário não encontrado com o email: " + email));
+        return repository.findByEmail(email).orElseThrow(() -> new RuntimeException("Usuário não encontrado com o email: " + email));
     }
 
 
     public void deletarUsuarioPorEmail(String email) {
 
         repository.deleteByEmail(email);
+    }
+
+    //Faça uma breve explicação do que o método abaixo faz
+    // O método atualizarUsuarioPorId recebe um ID e um objeto Usuario, busca o usuário existente pelo ID,
+    // e atualiza os campos email e nome com os valores do objeto Usuario, se eles não forem nulos.
+
+    public void atualizarUsuarioPorId(Integer id, Usuario usuario) {
+        Usuario usuarioEntity = repository.findById(Long.valueOf(id)).orElseThrow((() -> new RuntimeException("Usuário não encontrado com o ID: " + id)));
+        Usuario usuarioAtualizado = Usuario.builder()
+                .email(usuario.getEmail() != null ? usuario.getEmail() : usuarioEntity.getEmail())
+                .nome(usuario.getNome() != null ? usuario.getNome() : usuarioEntity.getNome())
+                .id(usuarioEntity.getId())
+                .build();
+        repository.saveAndFlush(usuarioAtualizado);
     }
 
 }
