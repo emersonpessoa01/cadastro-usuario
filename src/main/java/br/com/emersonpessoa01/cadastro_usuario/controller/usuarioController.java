34468665af73b1c69224a312c2b2d1a7b00d5b8e
emersonpessoa <emersonpessoa05102008@gmail.com>
2025-06-07 11:34:12 -0300

    feat: AddedatualizarUsuarioPorId :rocket:

--- src/main/java/br/com/emersonpessoa01/cadastro_usuario/controller/usuarioController.java
+++ src/main/java/br/com/emersonpessoa01/cadastro_usuario/controller/usuarioController.java
@@ -11,18 +11,30 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class usuarioController {
     private final UsuarioService usuarioService;
+
     @PostMapping
-    public ResponseEntity<Void> salvarUsuario(@RequestBody Usuario usuario){
+    public ResponseEntity<Void> salvarUsuario(@RequestBody Usuario usuario) {
         usuarioService.salvarUsuario(usuario);
         return ResponseEntity.ok().build();
     }
+
     @GetMapping
-    public ResponseEntity<Usuario> bucarUsuarioPorEmail(@RequestParam String email){
+    public ResponseEntity<Usuario> bucarUsuarioPorEmail(@RequestParam String email) {
         return ResponseEntity.ok(usuarioService.buscarUsuarioPorEmail(email));
     }
+
     @DeleteMapping
-    public ResponseEntity<Void> deletarUsuarioPorEmail(@RequestParam String email){
+    public ResponseEntity<Void> deletarUsuarioPorEmail(@RequestParam String email) {
         usuarioService.deletarUsuarioPorEmail(email);
         return ResponseEntity.ok().build();
     }
+
+    @PutMapping
+    public ResponseEntity<Void> atualizarUsuarioPorId(@RequestParam Integer id, @RequestBody Usuario usuario) {
+        usuarioService.atualizarUsuarioPorId(id, usuario);
+        return ResponseEntity.ok().build();
+    }
+
+
+}
 }
